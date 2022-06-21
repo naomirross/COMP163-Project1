55f57deb006fa00738b0d0d5949e34d289d1c3bd
naomirross <69229569+naomirross@users.noreply.github.com>
2022-06-21 18:15:03 -0400

    Create TestCase3.py

---
+++ TestCase3.py
@@ -0,0 +1,26 @@
+# Author: Naomi Ross
+# COMP163, Section 001
+#
+#
+# These input responses would result in 7 pickups.
+0.2
+61.3
+-1.2
+849.7
+0.8
+21.4
+5.4
+30.8
+4.8
+132.6
+6.5
+76.3
+9.6
+311.4
+-1.8
+88.9
+-0.3
+1401.0
+-6.9
+304.2
+# All Done. Total storage amount: 2631.2 cubic feet
